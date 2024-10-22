17b53324d06a75d5e4f040ada3a11638aabeb089
Petr Zabelin <zabelin.peter1216@gmail.com>
2024-10-22 23:25:08 +0300

    first

---
+++ GeometryUtils/src/main/java/com/example/geometry/utils/UnitConverter.java
@@ -0,0 +1,12 @@
+package com.example.geometry.utils;
+
+public class UnitConverter {
+
+    public static double cmToMeters(double cm) {
+        return cm / 100;
+    }
+
+    public static double metersToCm(double meters) {
+        return meters * 100;
+    }
+}
